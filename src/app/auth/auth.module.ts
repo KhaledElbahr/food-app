a40b3ac0b15100466b3b77ebeff0b42c5af612cf
Sara <sarakilany13@gmail.com>
2022-07-06 17:49:56 +0200

    feat: login reactive form with validators

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CheckboxModule } from 'primeng/checkbox';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { LoginComponent } from './login/login.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { DividerModule } from "primeng/divider";
+import { PasswordModule } from 'primeng/password';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    CheckboxModule,
+    InputTextModule,
+    DividerModule,
+    PasswordModule,
+
   ]
 })
 export class AuthModule { }
