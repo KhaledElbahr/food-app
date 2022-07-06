594fa02ac7399fca218039fb561cf3505d306a3c
KhaledElbahr <39977044+KhaledElbahr@users.noreply.github.com>
2022-07-06 18:25:33 +0200

    fix: typo imports

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CheckboxModule } from 'primeng/checkbox';
+import { AuthRoutingModule } from './auth-routing.module';
 
 import { ReactiveFormsModule } from '@angular/forms';
-import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+
 import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
 import { DividerModule } from "primeng/divider";
 import { PasswordModule } from 'primeng/password';
 
-@NgModule({
-  declarations: [
-    LoginComponent
-  ],
-import { RegisterComponent } from './register/register.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [RegisterComponent],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule,
@@ -29,8 +27,5 @@ import { InputTextModule } from 'primeng/inputtext';
     PasswordModule,
 
   ]
-    InputTextModule,
-  ],
-  exports: [RegisterComponent],
 })
 export class AuthModule {}
