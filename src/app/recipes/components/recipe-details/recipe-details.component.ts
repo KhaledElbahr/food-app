5dd7899e364539e12b1faf903e421ffc2d7be52b
mohamedrefat98 <refatmohamd98@gmail.com>
2022-07-08 02:20:22 +0200

    feat : making data shown in recipe list dynamic , edit : editting recipe interface and adding tow interfaces && edditing getting request funcs in the recipes service

--- src/app/recipes/components/recipe-details/recipe-details.component.ts
+++ src/app/recipes/components/recipe-details/recipe-details.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/shared/models/Recipe';
+import { RecipesService } from 'src/app/shared/services/recipes.service';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.scss']
+  styleUrls: ['./recipe-details.component.scss'],
 })
 export class RecipeDetailsComponent implements OnInit {
-
-  constructor() { }
+  recipe: Recipe = {
+    publisher: '',
+    title: '',
+    source_url: '',
+    recipe_id: '',
+    image_url: '',
+    social_rank: 0,
+    publisher_url: '',
+  };
+  id: string = '47746';
+  constructor(private recipeService: RecipesService) {}
 
   ngOnInit(): void {
+    this.recipeService.getRecipe(this.id).subscribe(
+      (data) => {
+        this.recipe = data.recipe;
+      },
+      (err) => console.log(err)
+    );
   }
-
 }
--- src/app/shared/models/Recipe.ts
+++ src/app/shared/models/Recipe.ts
@@ -1,11 +1,19 @@
 export interface Recipe {
-  publisher: string,
-  ingredients?: [],
-  title: string,
-  source_url: string
-  recipe_id: string,
-  image_url: string,
-  social_rank: number,
-  publisher_url: string
+  publisher: string;
+  ingredients?: string[];
+  title: string;
+  source_url: string;
+  recipe_id: string;
+  image_url: string;
+  social_rank: number;
+  publisher_url: string;
 }
 
+export interface SingleRecipe {
+  recipe: Recipe;
+}
+
+export interface RecipeList {
+  count: number;
+  recipes: Recipe[];
+}
